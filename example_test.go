c4d8b7290fe0bb3b4e1c1ba4661392d4253fc5a3
Pieter Droogendijk <pieter@binky.org.uk>
2014-03-18 17:36:56 +0100

    Added documentation and an example

---
+++ example_test.go
@@ -0,0 +1,143 @@
+package lexinator_test
+
+import (
+	"fmt"
+	"github.com/PieterD/lexinator"
+	"unicode"
+)
+
+const (
+	tokenComment lexinator.TokenType = 1 + iota
+	tokenVariable
+	tokenAssign
+	tokenNumber
+	tokenString
+)
+
+func ExampleLexer() {
+	text := `
+/* comment */
+pie=314
+// comment
+string = "Hello world!"
+`
+	l := lexinator.New("filename", text, state_base)
+	tokenchan := l.Go()
+	for token := range tokenchan {
+		fmt.Printf("%s:%d [%d]\"%s\"\n", token.File, token.Line, token.Typ, token.Val)
+	}
+	// Output: filename:2 [1]"/* comment */"
+	// filename:3 [2]"pie"
+	// filename:3 [3]"="
+	// filename:4 [4]"314"
+	// filename:6 [1]"// comment"
+	// filename:7 [2]"string"
+	// filename:7 [3]"="
+	// filename:7 [5]""Hello world!""
+	// filename:8 [-3]"EOF"
+}
+
+// Start parsing with this.
+func state_base(l *lexinator.Lexer) lexinator.StateFn {
+	// Ignore all whitespace.
+	l.Run(unicode.IsSpace)
+	l.Ignore()
+	if l.String("//") {
+		// We're remembering the '//' here so it gets included in the Emit
+		// contained in state_comment_line.
+		return state_comment_line
+	}
+	if l.String("/*") {
+		return state_comment_block(state_base)
+	}
+	if l.Eof() {
+		return l.EmitEof()
+	}
+	// It's not a comment or Eof, so it must be a variable name.
+	return state_variable
+}
+
+// Parse a line comment.
+func state_comment_line(l *lexinator.Lexer) lexinator.StateFn {
+	// Eat up everything until end of line (or Eof)
+	l.ExceptRun("\n")
+	l.Emit(tokenComment)
+	// Consume the end of line. If we reached Eof, this does nothing.
+	l.Accept("\n")
+	// Ignore that last newline
+	l.Ignore()
+	return state_base
+}
+
+// Parse a block comment.
+// Since block comments may appear in different states,
+// instead of defining the usual StateFn we define a function that
+// returns a statefn, which in turn will return the parent state
+// after its parsing is done.
+func state_comment_block(parent lexinator.StateFn) lexinator.StateFn {
+	return func(l *lexinator.Lexer) lexinator.StateFn {
+		if !l.Find("*/") {
+			// If closing statement couldn't be found, emit an error.
+			// Errorf always returns nil, so parsing is done after this.
+			return l.Errorf("Couldn't find end of block comment")
+		}
+		l.String("*/")
+		l.Emit(tokenComment)
+		return parent
+	}
+}
+
+// Parse a variable name
+func state_variable(l *lexinator.Lexer) lexinator.StateFn {
+	if l.AcceptRun("abcdefghijklmnopqrstuvwxyz") == 0 {
+		return l.Errorf("Invalid variable name")
+	}
+	l.Emit(tokenVariable)
+
+	return state_operator
+}
+
+// Parse an assignment operator
+func state_operator(l *lexinator.Lexer) lexinator.StateFn {
+	l.Run(unicode.IsSpace)
+	l.Ignore()
+	if l.Accept("=") {
+		l.Emit(tokenAssign)
+		return state_value
+	}
+	return l.Errorf("Only '=' is a valid operator")
+}
+
+// Parse a value
+func state_value(l *lexinator.Lexer) lexinator.StateFn {
+	l.Run(unicode.IsSpace)
+	l.Ignore()
+	if l.AcceptRun("0123456789") > 0 {
+		l.Emit(tokenNumber)
+		return state_base
+	}
+	if l.Accept("\"") {
+		return state_string
+	}
+	return l.Errorf("Unidentified value")
+}
+
+// Parse a string
+func state_string(l *lexinator.Lexer) lexinator.StateFn {
+	for {
+		l.ExceptRun("\"\\")
+		// Now we're either at a ", a \, or Eof.
+		if l.Accept("\"") {
+			l.Emit(tokenString)
+			return state_base
+		}
+		if l.Accept("\\") {
+			if !l.Accept("nrt\"'\\") {
+				return l.Errorf("Invalid escape sequence: \"\\%c\"", l.Last())
+			}
+		}
+		if l.Eof() {
+			return l.Errorf("No closing '\"' found")
+		}
+	}
+}
--- token.go
+++ token.go
@@ -2,6 +2,9 @@ package lexinator
 
 import "fmt"
 
+// Tokens are emitted by the lexer. They contained a (usually) user-defined
+// Typ, the Value of the token, and the Filename and Line number where the
+// token was generated.
 type Token struct {
 	Typ  TokenType
 	Val  string
@@ -9,15 +12,24 @@ type Token struct {
 	Line int
 }
 
+// TokenType is an integer representing the type of token that has been emitted.
+// Most TokenTypes will be userdefined, and those that are must be greater than 0.
 type TokenType int
 
 const (
+	// TokenEmpty is the TokenType with value 0.
+	// Any zero-valued token will have this as its Typ.
 	TokenEmpty TokenType = -iota
+	// TokenStopped is contained by tokens returned by Lexer.Token when
+	// the lexer has stopped (by an error, or Eof)
 	TokenStopped
+	// TokenError is the Typ for errors reported by, for example, Lexer.Errorf.
 	TokenError
+	// TokenEOF is returned once per file, when the end of file has been reached.
 	TokenEOF
 )
 
+// Return a simple string representation of the value contained within the token.
 func (i Token) String() string {
 	switch i.Typ {
 	case TokenError:
